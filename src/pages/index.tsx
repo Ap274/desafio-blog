c058fed082dd4bfe81e1f4e06fa47cedf9268440
Ap274 <aldopiresaps@gmail.com>
2023-05-27 13:10:40 -0300

    feat: prismic connection and post listing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
 import { GetStaticProps } from 'next';
-import Head from 'next/head'
-import Link from 'next/link'
-import { FiCalendar, FiUser } from 'react-icons/fi'
+import Head from 'next/head';
+import Link from 'next/link';
+import { ptBR } from 'date-fns/locale';
+import { useState } from 'react';
 
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
-import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -28,7 +31,47 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const formatPosts = postsPagination.results.map(post => ({
+    ...post,
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+      ),
+  }));
+
+  const [posts, setPosts] = useState<Post[]>(formatPosts);
+  //post list formatted
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  //stores the url of the next page of posts
+
+  async function handleNextPage(): Promise<void> {
+    if (!nextPage) return;
+
+    const postsResults = await fetch(nextPage).then(response => response.json());
+    // console.log(postsResults);
+
+    setNextPage(postsResults.next_page);
+
+    const newPosts = postsResults.results.map((post: Post) => ({
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    }));
+
+    if (newPosts && newPosts.length > 0) {
+      setPosts(prevPosts => [...prevPosts, ...newPosts]);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -39,56 +82,56 @@ export default function Home() {
         <Header />
 
         <section className={styles.pad}>
-          <Link href="">
-            <a className={styles.post}>
-              <h1>Como utilizar Hooks</h1>
-              <p>Pensando em sincronização em vez de ciclos de vida</p>
-              <div>
-                <FiCalendar />
-                <time>15 Mar 2021</time>
-                <FiUser />
-                <p>Joseph Oliveira</p>
-              </div>
-            </a>
-          </Link>
+          {posts.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a className={styles.post}>
+                <h1>{post.data.title}</h1>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <FiCalendar />
+                  <time>{post.first_publication_date}</time>
+                  <FiUser />
+                  <p>{post.data.author}</p>
+                </div>
+              </a>
+            </Link>
+          ))}
 
-          <Link href="">
-            <a className={styles.post}>
-              <h1>Como utilizar Hooks</h1>
-              <p>Pensando em sincronização em vez de ciclos de vida</p>
-              <div>
-                <FiCalendar />
-                <time>15 Mar 2021</time>
-                <FiUser />
-                <p>Joseph Oliveira</p>
-              </div>
-            </a>
-          </Link>
-
-          <Link href="">
-            <a className={styles.post}>
-              <h1>Como utilizar Hooks</h1>
-              <p>Pensando em sincronização em vez de ciclos de vida</p>
-              <div>
-                <FiCalendar />
-                <time>15 Mar 2021</time>
-                <FiUser />
-                <p>Joseph Oliveira</p>
-              </div>
-            </a>
-          </Link>
+          {nextPage && (
+            <button
+              className={styles.button}
+              type="button"
+              onClick={handleNextPage}
+            >
+              Carregar mais posts
+            </button>
+          )}
         </section>
-
-        <button className={styles.button}>Carregar mais posts</button>
       </main>
     </>
-  )
-  // TODO
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', {
+    pageSize: 1,
+    orderings: {
+      field: 'last_publication_date',
+      direction: 'desc',
+    },
+  });
 
-//   // TODO
-// };
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results,
+  };
+
+  console.log(postsPagination.next_page);
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
